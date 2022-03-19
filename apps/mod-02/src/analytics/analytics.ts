259e5b1e15931ba5de3bef8201362268ba0a578d
Phil Boyd <Phil.Boyd@csat.com>
2022-03-19 15:27:20 -0500

    wip(understanding-ts): chugging along

---
+++ apps/mod-02/src/analytics/analytics.ts
@@ -0,0 +1,5 @@
+console.info('sending info to analytics....refreshing additional text');
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', () => console.log('clicked'));
+
