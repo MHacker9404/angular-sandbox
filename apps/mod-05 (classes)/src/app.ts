000afc3f1242b9caa0aa218bf9c3a2904f865729
Phil Boyd <Phil.Boyd@csat.com>
2022-03-21 15:06:01 -0400

    wip(understanding-ts): mod-05 complete

--- apps/mod-05 (classes)/src/app.ts
+++ apps/mod-05 (classes)/src/app.ts
@@ -1,22 +1,48 @@
-class Department {
+interface OldPerson {
   name: string;
-  private _employees: string[] = [];
+  age: number;
 
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  public describe() {
-    console.log(`Department: ${this.name}`);
-  }
-
-  static createEmployee(name: string) {
-    return { name };
-  }
+  greet(phrase: string): void;
 }
 
-let accounting = new Department('Accounting');
-console.info(accounting);
-accounting.describe();
+let user1: OldPerson;
+user1 = {
+  name: 'Max',
+  age: 30,
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  },
+};
 
-console.info(Department.createEmployee('phil'));
+user1.greet('Hi there - I am');
+
+interface Named {
+  name: string;
+  outputName?: string;    //optional in extending/implementing classes
+}
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  constructor(public name: string) {}
+  greet = (phrase: string) => console.info(`${phrase} ${this.name}`);
+}
+
+let user2: Greetable;
+user2 = {
+  name: 'Max',
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  },
+};
+console.info(user2);
+user2 = new Person('Maui');
+
+type AddFn = (n1: number, n2: number) => number;
+interface IAddFn {
+  (n1: number, n2: number): number;
+}
+let add: IAddFn;
+add = (n1: number, n2: number) => n1 + n2;
+console.info(add(2, 3));
