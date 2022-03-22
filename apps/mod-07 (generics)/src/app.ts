b79f344162a177802410a4fad91f29dec27cd752
Phil Boyd <Phil.Boyd@csat.com>
2022-03-22 12:12:11 -0400

    wip(understanding-ts): mod-07 complete

--- apps/mod-07 (generics)/src/app.ts
+++ apps/mod-07 (generics)/src/app.ts
@@ -11,9 +11,32 @@ interface Lengthy {
 function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
   let descText = 'Got no value';
   if (element.length > 0) {
-    descText = `Got ${element.length} element(s)`
+    descText = `Got ${element.length} element(s)`;
   }
-  return [element, descText]
- }
+  return [element, descText];
+}
 console.info(countAndDescribe('Hi there'));
-console.info(countAndDescribe(['Sports','Cooking']));
+console.info(countAndDescribe(['Sports', 'Cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Value: ${obj[key]}`;
+}
+extractAndConvert({ name: 'phil' }, 'name');
+
+class TsStorage<T> {
+  private data:T[] = [];
+  addItem(item:T) {
+    this.data.push(item);
+  }
+  removeItem(item:T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems():T[] {
+    return [...this.data];
+  }
+}
+const stringStorage = new TsStorage<string>();
+stringStorage.addItem(10);
