7de66bc498824c5afafa2559365939f4b2b2ea71
Phil Boyd <Phil.Boyd@csat.com>
2022-03-22 19:38:25 -0500

    wip(understanding-ts): in-flight

--- apps/mod-08 (decorators)/src/app.ts
+++ apps/mod-08 (decorators)/src/app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+function Logger(targetConstructor: Function) {
+  console.info('logging....');
+  console.info(targetConstructor);
+}
+
+@Logger
+class Person {
+  name: string = 'phil';
+  constructor() {
+    console.info('creating person object');
+  }
+}
+
+const person = new Person();
+console.info(person);
