65e685978a2e3051397f68a02ed3a2c28d1260e0
Phil Boyd <phil.boyd@prb-services-llc.com>
2022-03-31 21:39:31 -0400

    wip(understanding-ts): mod-14 init

--- apps/mod-14/declarations.d.ts
+++ apps/mod-14/declarations.d.ts
@@ -1,6 +1,8 @@
+//xomino.com/2019/08/19/cannot-find-scss-module-error-enabling-sass-integration-with-your-sharepoint-framework-code/
+
 // We need to tell TypeScript that when we write "import styles from './styles.scss'
 // we mean to load a module(to look for a './styles.scss.d.ts').;
-declare module '*.scss' {
+https: declare module '*.scss' {
     const content: { [className: string]: string };
     export = content;
 }
