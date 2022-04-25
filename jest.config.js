da0281e22e489527bc40e9eae1c49b3cd3251b28
Phil Boyd <Phil.Boyd@csat.com>
2022-04-25 09:46:04 -0400

    wip(repo): project init

--- jest.config.js
+++ jest.config.js
@@ -3,3 +3,32 @@ const { getJestProjects } = require('@nrwl/jest');
 module.exports = {
   projects: getJestProjects(),
 };
+
+// {
+//   "globals": {
+//     "ts-jest": {
+//       "tsConfigFile": "tsconfig.spec.json"
+//     }
+//   },
+//   "preset": "jest-preset-angular",
+//   "roots": [
+//     "<rootDir>/apps/",
+//     "<rootDir>/libs/"
+//   ],
+//   "collectCoverage":true
+//   ,
+//   "collectCoverageFrom": [
+//     "**/*.{ts}",
+//     "!**/node_modules/**",
+//     "!**/vendor/**",
+//     "!jestGlobalMocks.ts",
+//     "!setupJest.ts",
+//     "!**/test.ts",
+//     "!**/index.ts"
+//   ],
+//   "setupFiles": [
+//     "whatwg-fetch",
+//     "jest-localstorage-mock"
+//   ],
+//   "setupTestFrameworkScriptFile": "./setupJest.ts"
+// }
