9f0a83617242830d182a6d297123883f6a15bba2
Phil Boyd <Phil.Boyd@csat.com>
2022-04-26 09:56:52 -0400

    chore(repo): mod init && yarn upgrade-interactive

---
+++ apps/unit-testing-jest/jest.config.ts
@@ -0,0 +1,21 @@
+module.exports = {
+    displayName: 'unit-testing-jest',
+    preset: '../../jest.preset.ts',
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.(html|svg)$'
+        }
+    },
+    coverageDirectory: '../../coverage/apps/unit-testing-jest',
+    transform: {
+        '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
+    },
+    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+    snapshotSerializers: [
+        'jest-preset-angular/build/serializers/no-ng-attributes',
+        'jest-preset-angular/build/serializers/ng-snapshot',
+        'jest-preset-angular/build/serializers/html-comment'
+    ]
+};
