e8b1331134f1850ad2897a1bb636a2a81cb1d4ff
Phil Boyd <Phil.Boyd@csat.com>
2022-04-27 11:59:30 -0400

    wip(unit-testing): init unit-testing module

--- apps/unit-testing-jest/src/app/app.module.ts
+++ apps/unit-testing-jest/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent],
+    declarations: [AppComponent, UserComponent],
     imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' })],
     providers: [],
     bootstrap: [AppComponent]
