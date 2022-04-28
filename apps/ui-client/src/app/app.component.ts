d720a9f16f6dc2d670c4a8aaf4dac29696b0f353
Phil Boyd <Phil.Boyd@csat.com>
2022-04-28 17:51:28 -0400

    wip(au-testing-masterclass): fix build issues

--- apps/ui-client/src/app/app.component.ts
+++ apps/ui-client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'nx-apps-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'ui-client';
+  title = 'Angular Testing Course';
 }
