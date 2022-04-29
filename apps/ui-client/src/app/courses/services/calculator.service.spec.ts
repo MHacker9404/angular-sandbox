4be6c44e2c37d365b8a3d9404aa34e8aca373009
Phil Boyd <phil.boyd@prb-services-llc.com>
2022-04-29 15:48:07 -0400

    wip(au-testing-masterclass): in-flight

--- apps/ui-client/src/app/courses/services/calculator.service.spec.ts
+++ apps/ui-client/src/app/courses/services/calculator.service.spec.ts
@@ -4,40 +4,40 @@ import { TestBed } from '@angular/core/testing';
 import "jest";
 
 describe('CalculatorService', () => {
-    let calculator: CalculatorService, loggerSpy: any;
+    // let calculator: CalculatorService, loggerSpy: any;
 
-    beforeEach(() => {
-        console.log('Calling beforeEach');
+    // beforeEach(() => {
+    //     console.log('Calling beforeEach');
 
-        loggerSpy = jest.spyOn(LoggerService, 'log');
+    //     loggerSpy = jest.spyOn(LoggerService, 'log');
 
-        TestBed.configureTestingModule({
-            providers: [
-                CalculatorService,
-                { provide: LoggerService, useValue: loggerSpy }
-            ]
-        });
+    //     TestBed.configureTestingModule({
+    //         providers: [
+    //             CalculatorService,
+    //             { provide: LoggerService, useValue: loggerSpy }
+    //         ]
+    //     });
 
-        calculator = TestBed.get(CalculatorService);
-    });
+    //     calculator = TestBed.get(CalculatorService);
+    // });
 
-    it('should add two numbers', () => {
-        console.log('add test');
+    // it('should add two numbers', () => {
+    //     console.log('add test');
 
-        const result = calculator.add(2, 2);
+    //     const result = calculator.add(2, 2);
 
-        expect(result).toBe(4);
+    //     expect(result).toBe(4);
 
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-    });
+    //     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    // });
 
-    it('should subtract two numbers', () => {
-        console.log('subtract test');
+    // it('should subtract two numbers', () => {
+    //     console.log('subtract test');
 
-        const result = calculator.subtract(2, 2);
+    //     const result = calculator.subtract(2, 2);
 
-        expect(result).toBe(0, 'unexpected subtraction result');
+    //     expect(result).toBe(0, 'unexpected subtraction result');
 
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-    });
+    //     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    // });
 });
